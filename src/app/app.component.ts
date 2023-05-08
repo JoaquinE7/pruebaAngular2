abc51111277397e1962f4796023f3d741510b167
JoaquinE7 <joaquin.miguel2002@gmail.com>
2023-05-07 19:47:47 -0600

    aaaa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe';
-
-  video='u5TuM21ZBQg';
+  title = 'Peliculiando';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DomseguroPipe } from './domseguro.pipe';
+import { NavModule } from './shared/components/nav/nav.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DomseguroPipe
+    AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    NavModule,
+    HttpClientModule,
+    MatSlideToggleModule,
+    MatCardModule
   ],
+  exports:[],
   providers: [],
   bootstrap: [AppComponent]
 })
